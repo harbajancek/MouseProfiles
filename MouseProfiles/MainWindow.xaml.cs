29eea25f4ba3ad2a638ef3cf861caa8a2f648619
Jan H <honzah70@outlook.cz>
2019-04-17 12:26:29 +0200

    test

--- MouseProfiles/MainWindow.xaml.cs
+++ MouseProfiles/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace MouseProfiles
 {
@@ -23,6 +25,46 @@ namespace MouseProfiles
         public MainWindow()
         {
             InitializeComponent();
+
+            NotifyIcon ni = new NotifyIcon();
+            ni.Icon = new Icon("Main.ico");
+            ni.Visible = true;
+            ni.Click += NotifyIcon_Click;
+        }
+
+        private void NotifyIcon_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Minimized;
+                return;
+            }
+            this.Show();
+            this.WindowState = WindowState.Normal;
+            System.Windows.Point p = GetMousePositionWindowsForms();
+            this.Top = p.Y - this.ActualHeight;
+            this.Left = p.X - this.ActualWidth;
+            this.Focus();
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                this.Hide();
+            }
+            base.OnStateChanged(e);
+        }
+        public static System.Windows.Point GetMousePositionWindowsForms()
+        {
+            System.Drawing.Point point = System.Windows.Forms.Control.MousePosition;
+            return new System.Windows.Point(point.X, point.Y);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.WindowState = WindowState.Minimized;
         }
     }
 }
